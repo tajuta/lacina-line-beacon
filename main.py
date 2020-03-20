4281877eb620d30a0855e37b4528d23d132a4963
shoichiro.tajuta <tajuta@gmail.com>
2020-03-20 09:51:34 +0900

    ビーコンを検出するように修正

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, send_file
 import os
 
 from linebot import (
@@ -7,11 +7,14 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
+
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, BeaconEvent,
 )
 
 app = Flask(__name__)
+statusDict  = {}
+status = 0
 
 #環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
@@ -44,10 +47,18 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    event.reply_token,[
+        TextSendMessage(text=event.message.text),
+    ])
+
+@handler.add(BeaconEvent)
+def handle_beacon(event):
+    print(event)
+    line_bot_api.reply_message(
+        event.reply_token,[
+            TextSendMessage(text='beaconを検出しました. event.type={}, hwid={}, device_message(hex string)={}'.format(event.beacon.type, event.beacon.hwid, event.beacon.dm)),
+        ])
 
 if __name__ == "__main__":
-#    app.run()
     port = int(os.getenv("PORT"))
     app.run(host="0.0.0.0", port=port)
