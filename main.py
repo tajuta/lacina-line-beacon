2d8aff80a9c288f8791ad5517c8351c5a49d8222
shoichiro.tajuta <tajuta@gmail.com>
2020-03-24 13:49:50 +0900

    先生を召喚するキーワードを「先生」「話したい」のいずれかにするように修正

--- main.py
+++ main.py
@@ -67,14 +67,14 @@ def handle_message(event):
     slack_info = slackweb.Slack(url=WEB_HOOK_LINKS)
 
     # 先生を召喚する
-    if event.message.text in "先生と話したい":
+    if event.message.text in "先生" or event.message.text in "話したい":
         line_bot_api.reply_message(
         event.reply_token,[
             TextSendMessage(text="先生を呼び出しているのでちょっとまってね。（すぐにお返事できない場合があるよ）"),
         ])
         # Slackにメッセージを送信
-        send_msg = "[{user_name}] {message}\n".format(user_name=user_name, message=event.message.text) \
-                + "<!channel> {user_name}さんが先生と話したがっています。LINE Official Accountの設定をチャットモードに切り替えて対応してください。".format(user_name=user_name)
+        send_msg = "<!channel> {user_name}さんが先生と話したがっています。LINE Official Accountの設定をチャットモードに切り替えて対応してください。".format(user_name=user_name) \
+                + "[{user_name}] {message}\n".format(user_name=user_name, message=event.message.text) 
         slack_info.notify(text=send_msg)
     # Talk APIを使って会話する
     else:
