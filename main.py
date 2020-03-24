d982f5040d5d0903b20edaf485ba3eb6c714bb91
shoichiro.tajuta <tajuta@gmail.com>
2020-03-24 13:29:36 +0900

    メンションの記法を修正

--- main.py
+++ main.py
@@ -73,8 +73,8 @@ def handle_message(event):
             TextSendMessage(text="先生を呼び出しているのでちょっとまってね。（すぐにお返事できない場合があるよ）"),
         ])
         # Slackにメッセージを送信
-        send_msg = "[user_name] {message}\n".format(user_name=user_name, message=event.message.text) \
-                + "@channel {user_name}さんが先生と話したがっています。LINE Official Accountの設定をチャットモードに切り替えて対応してください。".format(user_name=user_name)
+        send_msg = "[{user_name}] {message}\n".format(user_name=user_name, message=event.message.text) \
+                + "<!channel> {user_name}さんが先生と話したがっています。LINE Official Accountの設定をチャットモードに切り替えて対応してください。".format(user_name=user_name)
         slack_info.notify(text=send_msg)
     # Talk APIを使って会話する
     else:
